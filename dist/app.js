e3269064f7dfebbdc794f071036ab3b69222a757
Amod Suman <amodsumann@gmail.com>
2023-07-13 20:41:51 +0545

    Header & Footer - Completed

--- dist/app.js
+++ dist/app.js
@@ -5,9 +5,11 @@ const hammenubox = document.getElementById("hammenu");
 openHam.addEventListener("click", function () {
   hammenubox.style.visibility = "visible";
   hammenubox.style.opacity = "1";
+  document.body.style.overflow = "hidden";
 });
 
 closeHam.addEventListener("click", function () {
   hammenubox.style.visibility = "hidden";
   hammenubox.style.opacity = "0";
+  document.body.style.overflow = "auto";
 });
